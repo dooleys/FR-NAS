d36456757758a287f7d2bd13969a41f225fe2914
Samuel Dooley <sdooley1@cmlsub00.umiacs.umd.edu>
2022-04-26 15:24:24 -0400

    don't repeat runs in phase1biii that have occured in phase1bii

--- make_phase1biii.sh
+++ make_phase1biii.sh
@@ -1,4 +1,7 @@
 
 for f in configs_multi/**/*.yaml; do
-  echo "python python src/fairness_train_timm.py --config_path $f"
+  default=$(echo $f | sed 's/configs_multi/configs/g')
+  if ! [[ -f $default ]];then
+    echo "python python src/fairness_train_timm.py --config_path $f"
+  fi
 done 
