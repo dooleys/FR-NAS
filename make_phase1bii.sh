d630aa8d31f027820951c123c66160d896f6d132
Samuel Dooley <sdooley1@cmlsub00.umiacs.umd.edu>
2022-04-26 15:30:37 -0400

    update

--- make_phase1bii.sh
+++ make_phase1bii.sh
@@ -1,4 +1,4 @@
 
 for f in configs/**/*.yaml; do
-  echo "python python src/fairness_train_timm.py --config_path $f"
+  echo "python src/fairness_train_timm.py --config_path $f"
 done 
--- make_phase1biii.sh
+++ make_phase1biii.sh
@@ -2,6 +2,6 @@
 for f in configs_multi/**/*.yaml; do
   default=$(echo $f | sed 's/configs_multi/configs/g')
   if ! [[ -f $default ]];then
-    echo "python python src/fairness_train_timm.py --config_path $f"
+    echo "python src/fairness_train_timm.py --config_path $f"
   fi
 done 
