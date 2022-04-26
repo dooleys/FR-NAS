d9d200be4cd4ddd940002efc0b1a1c795317556b
Samuel Dooley <sdooley1@cmlsub00.umiacs.umd.edu>
2022-04-25 21:55:58 -0400

    added some of samuel's phase1b commands and config files

---
+++ phase1b_samuel.sh
@@ -0,0 +1,12 @@
+python create_configs.py --user_config config_user.yaml --backbone tnt_s_patch16_224 --batch_size 64 --sched cosine --epochs 300 --opt adamw  --warmup-lr 1e-6 --model-ema --model-ema-decay 0.99996 --warmup-epochs 5 --opt-eps 1e-8 --lr 1e-3 --weight-decay .05 --drop 0 --drop-path .1
+python create_configs.py --user_config config_user.yaml --backbone inception_resnet_v2 --batch_size 64 --lr 0.045 --lr-cycle-decay 0.94 --momentum 0.9 #--optimizer RMSProp
+python create_configs.py --user_config config_user.yaml --backbone rexnet_200 --batch_size 64 --lr 0.5 --momentum 0.9 --weight-decay 1e-5 #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone ig_resnext101_32x16d --batch_size 64 --lr 0.1 --momentum 0.9 --weight-decay 1e-4 --sched step #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone resnetrs420 --batch_size 64 --momentum 0.9 --lr 1.6 #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone selecsls60b --batch_size 64
+python create_configs.py --user_config config_user.yaml --backbone Fbnetv3_g --batch_size 64
+python create_configs.py --user_config config_user.yaml --backbone legacy_senet154 --batch_size 64 --lr 0.6 --momentum 0.9 --weight-decay 1e-5 #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone vgg19_bn --batch_size 64 --lr 0.1 --momentum 0.9 --weight-decay 1e-4 --sched step #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone vgg19 --batch_size 64 --lr 0.01 --momentum 0.9 --weight-decay 1e-4 --sched step #--optimizer SGD
+python create_configs.py --user_config config_user.yaml --backbone vit_large_patch16_224 --batch_size 64 --lr 0.5 --warmup-epochs 5 --weight-decay 0.00002 --clip-grad 1 --model-ema #--optimizer AdamW 
+python3 create_configs.py --user_config config_user.yaml --backbone mobilenetv3_large_100 --batch_size 64 --sched step --decay-epochs 2.4 --decay-rate .973 --opt-eps .001 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-connect 0.2 --model-ema --model-ema-decay 0.9999 --lr .064 --lr-noise 0.42 0.9 #--opt RMSpropTF
