0f8510c676d1f308c726ab071afeedc8f2dd5cda
Rhea Sukthanker <rheasukthanker@gmail.com>
2023-01-24 06:58:57 +0100

    add train from scratch scripts

--- src/search/dpn_objective.py
+++ src/search/dpn_objective.py
@@ -83,6 +83,7 @@ def fairness_objective_dpn(config, seed, budget):
     with open("/work/dlclarge2/sukthank-ZCP_Competition/FR-NAS/vgg_configs/configs_default/dpn107/config_dpn107_CosFace_sgd.yaml","r") as ymlfile:
         args = yaml.load(ymlfile, Loader=yaml.FullLoader)
     args = dotdict(args)
+    args.epochs = int(budget)
     args.opt = config["optimizer"]
     args.head = config["head"]
     print(args)
@@ -106,7 +107,6 @@ def fairness_objective_dpn(config, seed, budget):
     dataloaders, num_class, demographic_to_labels_train, demographic_to_labels_val = prepare_data(
         args)
     args.num_class = 7058
-    args.epochs=10
     edges=[int(config['edge1']),int(config['edge2']),int(config['edge3'])]
     # Build model
     backbone = DPN(edges,num_init_features=128, k_r=200, groups=50, k_sec=(1,1,1,1), inc_sec=(20, 64, 64, 128))
@@ -124,6 +124,7 @@ def fairness_objective_dpn(config, seed, budget):
         args.lr=config["lr_sgd"]
     print(args.lr)
     print(args.opt)
+
     optimizer = create_optimizer_v2(model, **optimizer_kwargs(cfg=args))
     scheduler, num_epochs = create_scheduler(args, optimizer)
     model.to(device)
@@ -180,7 +181,7 @@ def fairness_objective_dpn(config, seed, budget):
     df = {"ids":list(indices_all_val), "rank_by_id":list(np.array(rank_val[:,1].cpu().detach().numpy())), "gender_expression": list(demographic_all_val)}
     rank_by_id_val = pd.DataFrame(data=df)
     df_val = rank_by_id_val
-    rank_diff_val=rank_func(df_val)/(1+rank_func(df_val))
+    rank_diff_val=rank_func(df_val)#/(1+rank_func(df_val))
     acc_val=acc_overall(df_val)
     return {
         "rev_acc": 1-(acc_val), 
